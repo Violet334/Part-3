eb1b46fda440f51196be14c5c49d6d1c548308c1
livio <livio@OA-S318-19>
2024-04-01 12:01:58 -0400

    created parent script functions

--- Assets/Assignment/Scripts/NPC.cs
+++ Assets/Assignment/Scripts/NPC.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    Rigidbody2D rb;
+    protected Vector2 destination;
+    public Transform altDestination;
+    protected Vector2 movement;
+    float speed = 3;
+    protected Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        destination = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        movement = destination - (Vector2)transform.position;
+
+        //flip the x direction of the game object & children to face the direction we're walking
+        if (movement.x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (movement.x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        //stop moving if we're close enough to the target
+        if (movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
+
+    public virtual void Attack()
+    {
+
+    }
+
+    public virtual void Affection()
+    {
+
     }
 }
